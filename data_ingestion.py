39657a6036f2c6536fb5d826ef163b031ee18420
MasterLeg <emilio.p.221@gmail.come>
2025-02-02 13:32:52 +0100

    Fixed not ZIP file error

--- data_ingestion.py
+++ data_ingestion.py
@@ -6,12 +6,26 @@ import zipfile
 from utils import create_folder
 
 def download_source_files(input_url: str, output_path: Path):
+
+    # Create output path
+    create_folder(output_path)
+
+    print('Downloading file: ', input_url)
     response: requests.Response = requests.get(input_url)
     zip_content: bytes = response.content
 
-    unzipped_content: zipfile.ZipFile = zipfile.ZipFile(io.BytesIO(zip_content))
+    try:
+        print('Extracting ZIP file')
+        unzipped_content: zipfile.ZipFile = zipfile.ZipFile(io.BytesIO(zip_content))
+        unzipped_content.extractall(output_path)
 
-    create_folder(output_path)
+    except zipfile.BadZipFile:
+        print('Not a ZIP file')
+        output_path = output_path.parent/(output_path.name + '.bin')
+        with open(output_path, 'wb') as output_file:
+            output_file.write(zip_content)
 
-    unzipped_content.extractall(output_path)
-    print('Extrcted files on ', output_path)
+    print('Saved files on ', output_path)
+
+def scrap_metadata() -> None:
+    pass
