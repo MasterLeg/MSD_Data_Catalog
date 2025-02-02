397e6dfc310334faad0a175f6b185fcbbeb3a9e4
MasterLeg <emilio.p.221@gmail.come>
2025-02-02 13:33:16 +0100

    Generalized for everyt schema on config file

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ from data_ingestion import download_source_files
 def main() -> None:
     config_file_content: dict = load_config_file()
     PROJECT_PATH: Path = Path(os.getcwd())
+    
+    # Download the source files
+    for schema_source, details in config_file_content['sources'].items():
+        print('Executing for source: ', schema_source)
 
-    for schema_source in config_file_content['sources']:
-
-        output_file_path: str = PROJECT_PATH/schema_source['output']['folder_name']
-        input_url: str = schema_source['url']
+        output_file_path: str = PROJECT_PATH/details['output']['folder_name']
+        input_url: str = details['url']
 
         download_source_files(input_url, output_file_path)
 
