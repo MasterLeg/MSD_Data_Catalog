f0abc08f136995943521136602dd0b321fd3f5df
MasterLeg <emilio.pardo@takeda.com>
2025-02-13 10:24:53 +0100

    Connect streamlit to DuckDb

--- web_page.py
+++ web_page.py
@@ -1,16 +1,27 @@
 import streamlit as st
 
+import duckdb
 import pandas as pd
 import numpy as np
 
 
 def launch_web_page() -> None:
+
+    DUCKDB_FILE_PATH: str = 'downloads\\myduckdb.db'
+    # Create DuckDB connection
+    connection = duckdb.connect(DUCKDB_FILE_PATH)
+
     st.write("""
         # Web example 1
             Ejemplo de página web""")
     
-    df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
+    # df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
+    output_file: str = r'C:\Projects\MSD_Data_Catalog\downloads\pokedex\pokemon_bw.csv'
+    df = pd.DataFrame(connection.execute(f"SELECT * FROM read_csv('{output_file}')").df().dtypes)
+
     st.dataframe(df)
+    connection.close()
+
 
 if __name__ == '__main__':
     launch_web_page()
